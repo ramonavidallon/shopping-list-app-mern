727dfad9ebcb0d9a3ae2b81618b109ecf1802226
Ramon Vidallon <ramon.vidallon@icloud.com>
2019-09-20 12:16:44 -0700

    first push

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://testClient:<Aliale608>@tutorialcluster-shard-00-00-fchaj.mongodb.net:27017,tutorialcluster-shard-00-01-fchaj.mongodb.net:27017,tutorialcluster-shard-00-02-fchaj.mongodb.net:27017/test?ssl=true&replicaSet=TutorialCluster-shard-0&authSource=admin&retryWrites=true&w=majority'
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err));
